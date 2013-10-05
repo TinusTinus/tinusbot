94c8a4fdccb711f63e0268dc2ebbaeda7a316b86
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-10-05 19:16:34 +0200

    Javadoc.

--- devnobot-parent/devnobot-client/src/main/java/nl/mvdr/model/Tank.java
+++ devnobot-parent/devnobot-client/src/main/java/nl/mvdr/model/Tank.java
@@ -11,21 +11,6 @@ import com.cgi.devnobot.api.Orientation;
 /**
  * Represents a tank in the game world.
  * 
- * Contains the status of a certain bot (tank).
- * 
- * - lastKnownOrientation is useful to determine which direction a tank is facing (and might possibly fire) -
- * queueLength every tank has a queue of tasks to perform. It is advisable to keep this queue close to empty. Every
- * action is added to the queue and the game-engine will poll the items from this queue. To remain agile/flexible it is
- * advisable to not add more then 3 items in your queue (it is not possible to clear the queue or cancel items). If you
- * add items to fast it will result in a tank that won't respond for a while to new events (because it first will
- * process all earlier submitted actions)
- * 
- * Some fields are static (they don't change while running the game or amongst other bots): - actionDurationInMs => this
- * is the time it takes to move forward/backward/turn left/ turn right (shooting is more time consuming) this can be
- * used to calculate the time it takes to get somewhere. - distancePerStep => the amount of 'pixels' that a tank moves
- * for one action of type forward/backward when the gap between your tank and the walls is smaller then this. it will
- * drive up to the wall (no collision damage) </pre>
- * 
  * @author Martijn van de Rijdt
  */
 @Getter
