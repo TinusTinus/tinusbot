c8e2d7f81893c25cf26a38dfb75cee54283a0344
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-10-05 20:09:46 +0200

    Added Wall.

---
+++ devnobot-parent/devnobot-client/src/main/java/nl/mvdr/model/Wall.java
@@ -0,0 +1,37 @@
+package nl.mvdr.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import com.cgi.devnobot.api.GameObstacle;
+
+/**
+ * Representation of a wall / obstacle.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Wall extends GameObject {
+    /**
+     * Constructor.
+     * 
+     * @param obstacle
+     *            game object as received from the client api
+     */
+    public Wall(GameObstacle obstacle) {
+        super(obstacle);
+    }
+    
+    /**
+     * Constructor. Intended for use in unit tests.
+     * 
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param width width
+     * @param height height
+     */
+    Wall(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+}
---
+++ devnobot-parent/devnobot-client/src/test/java/nl/mvdr/model/WallTest.java
@@ -0,0 +1,35 @@
+package nl.mvdr.model;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test class for {@link Wall}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Slf4j
+public class WallTest {
+    /** Test method for {@link Wall#toString()}. */
+    @Test
+    public void testToString() {
+        Wall wall = new Wall(0, 1, 2, 3);
+        
+        String string = wall.toString();
+        
+        log.info(string);
+        Assert.assertNotNull(string);
+        Assert.assertNotEquals("", string);
+    }
+    
+    /** Tests whether equals can tell that walls and bullets are not the same thing. */
+    @Test
+    public void testEquals() {
+        Wall wall = new Wall(0, 1, 2, 3);
+        Bullet bullet = new Bullet(0, 1, 2, 3);
+        
+        Assert.assertNotEquals(wall, bullet);
+    }
+}
