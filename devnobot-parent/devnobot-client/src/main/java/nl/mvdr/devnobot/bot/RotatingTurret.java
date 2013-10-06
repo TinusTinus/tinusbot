a989ace2b04ce109da2519439c47db710ddc2d10
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-10-06 15:10:35 +0200

    Added another fairly dumb bot implementation.

---
+++ devnobot-parent/devnobot-client/src/main/java/nl/mvdr/devnobot/bot/RotatingTurret.java
@@ -0,0 +1,41 @@
+package nl.mvdr.devnobot.bot;
+
+import java.awt.Color;
+import java.util.Collection;
+
+import nl.mvdr.devnobot.clientapi.ClientApi;
+import nl.mvdr.devnobot.model.Action;
+import nl.mvdr.devnobot.model.GameState;
+import nl.mvdr.devnobot.model.Wall;
+
+/**
+ * Alternately fires and turns.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class RotatingTurret extends BotArtificialIntelligence {
+    /** Previous action. */
+    private Action previousAction;
+    
+    /**
+     * Constructor.
+     * 
+     * @param clientApi api
+     */
+    public RotatingTurret(ClientApi clientApi) {
+        super(clientApi, "Rotating Turret", Color.LIGHT_GRAY);
+        previousAction = null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected Action determineNextAction(Collection<Wall> obstacles, GameState state) {
+        if (previousAction == Action.FIRE) {
+            previousAction = Action.TURN_RIGHT;
+        } else {
+            previousAction = Action.FIRE;
+        }
+        return previousAction;
+    }
+
+}
