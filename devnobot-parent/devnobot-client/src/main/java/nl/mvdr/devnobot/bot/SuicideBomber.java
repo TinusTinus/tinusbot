5d9eadc1c326ee20bd8110d1e0997aff76e3d444
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-10-06 13:52:45 +0200

    Added a bot that moves around by killing itself.

---
+++ devnobot-parent/devnobot-client/src/main/java/nl/mvdr/devnobot/bot/SuicideBomber.java
@@ -0,0 +1,41 @@
+package nl.mvdr.devnobot.bot;
+
+import java.awt.Color;
+import java.util.Collection;
+
+import nl.mvdr.devnobot.clientapi.ClientApi;
+import nl.mvdr.devnobot.model.Action;
+import nl.mvdr.devnobot.model.GameState;
+import nl.mvdr.devnobot.model.Wall;
+
+/**
+ * Suicide bomber artificial intelligence. Alternately fires and suicides.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class SuicideBomber extends BotArtificialIntelligence {
+    /** Previous action. */
+    private Action previousAction;
+    
+    /**
+     * Constructor.
+     * 
+     * @param clientApi api
+     */
+    public SuicideBomber(ClientApi clientApi) {
+        super(clientApi, "Suicide Bomber", Color.RED);
+        previousAction = null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected Action determineNextAction(Collection<Wall> obstacles, GameState state) {
+        if (previousAction == Action.FIRE) {
+            previousAction = Action.SUICIDE;
+        } else {
+            previousAction = Action.FIRE;
+        }
+        return previousAction;
+    }
+
+}
