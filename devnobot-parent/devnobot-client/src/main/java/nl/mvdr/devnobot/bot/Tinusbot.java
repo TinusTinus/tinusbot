bc6e3ba3665e4ab0254f06cf148283f162cec75b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-10-06 17:21:28 +0200

    Suicide when opponents are aiming at us.

--- devnobot-parent/devnobot-client/src/main/java/nl/mvdr/devnobot/bot/Tinusbot.java
+++ devnobot-parent/devnobot-client/src/main/java/nl/mvdr/devnobot/bot/Tinusbot.java
@@ -2,10 +2,12 @@ package nl.mvdr.devnobot.bot;
 
 import java.awt.Color;
 import java.util.Collection;
+import java.util.Iterator;
 
 import nl.mvdr.devnobot.clientapi.ClientApi;
 import nl.mvdr.devnobot.model.Action;
 import nl.mvdr.devnobot.model.GameState;
+import nl.mvdr.devnobot.model.Tank;
 import nl.mvdr.devnobot.model.Wall;
 
 /**
@@ -44,9 +46,33 @@ public class Tinusbot extends BotArtificialIntelligence {
         Action result;
         if (state.wouldHitEnemy(getName(), obstacles)) {
             result = Action.FIRE;
+        } else if (nonDummyEnemyHasAShot(obstacles, state)) {
+            // Suicide to prevent them from getting the kill.
+            // TODO test if this actually helps!
+            result = Action.SUICIDE;
         } else {
             // TODO move to a position where we can fire?
             result = null;
+        }
+        return result;
+    }
+    
+    /**
+     * Determines whether a non-dummy enemy is aiming at our tank.
+     * 
+     * @param obstacles
+     *            walls
+     * @param state
+     *            game state
+     * @return whether a bot is aiming at us
+     */
+    private boolean nonDummyEnemyHasAShot(Collection<Wall> obstacles, GameState state) {
+        boolean result = false;
+        Tank ownTank = state.retrieveTankForPlayerName(getName());
+        Iterator<Tank> enemyIterator = state.retrieveEnemies(getName()).iterator();
+        while (!result && enemyIterator.hasNext()) {
+            Tank enemy = enemyIterator.next();
+            result = !enemy.isProbablyADummy() && state.wouldHit(enemy.getPlayer(), obstacles) == ownTank;
         }
         return result;
     }
